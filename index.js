50e52327315cdeaf1a201915c2d4a1d50dd942ed
Blaisosoft <blaisemunezero9@gmail.com>
2023-05-16 13:34:53 -0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-let tableau = [500,3,12,15,2,96,35,65,15,78,15]
-let tableauTrie = tableau.sort((a,b)=>{
-    return a-b
+let distance = [3, 3, 3, 3, 2, 2, 2, 6, 6, 9, 12]
+let customerSchema = []
+let distanceByArrive = distance.sort((a, b) => {
+  return a - b
 })
 
+console.log(distanceByArrive)
+var matrice = new Array()
+
+for (
+  let index = 0;
+  index <= distanceByArrive[distanceByArrive.length - 1];
+  index++
+) {
+  matrice[index] = index
+  let result = distanceByArrive.filter(
+    (distanceCurrent) => distanceCurrent == index
+  )
+  if (result.length != 0) {
+    for (let j = 0; j < result.length; j++) {
+      console.log(
+        `${j + 1}` + " " + `client${j > 1 ? "s" : ""} arrive`,
+        result[j] + "km"
+      )
     }
+    // result = []
+    console.log("=======================================")
   }
 }
