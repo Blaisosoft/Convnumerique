83ee1fdde41750c33931d6a40858aaf1e2922a38
Blaisosoft <blaisemunezero9@gmail.com>
2023-05-16 14:16:38 -0700

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
-let distance = [3, 3, 3, 3, 2, 2, 2, 6, 6, 9, 12]
-let customerSchema = []
-let distanceByArrive = distance.sort((a, b) => {
-  return a - b
+let distance = [3, 3, 3, 3, 2, 2, 2, 6, 6, 9, 11, 11, 13, 12]
+let customerSchema = [
+  {
+    name: "Blaise",
+    placeNumber: 2,
+    depart: "Musaga",
+    destination: "jenda",
+    distance: 45,
+  },
+  {
+    name: "Patrick",
+    placeNumber: 10,
+    depart: "Musaga",
+    destination: "Rutovu",
+    distance: 110,
+  },
+  {
+    name: "Erica",
+    placeNumber: 1,
+    depart: "Musaga",
+    destination: "Mahwa",
+    distance: 100,
+  },
+  {
+    name: "Arsène",
+    placeNumber: 5,
+    depart: "Musaga",
+    destination: "Matana",
+    distance: 90,
+  },
+  {
+    name: "Arsène",
+    placeNumber: 8,
+    depart: "Musaga",
+    destination: "Matana",
+    distance: 90,
+  },
+  {
+    name: "Kevine",
+    placeNumber: 18,
+    depart: "Musaga",
+    destination: "Rutovu",
+    distance: 110,
+  },
+  {
+    name: "Rodrigue",
+    placeNumber: 11,
+    depart: "Musaga",
+    destination: "Nyakararo",
+    distance: 80,
+  },
+  {
+    name: "Charmant",
+    placeNumber: 3,
+    depart: "Musaga",
+    destination: "Rutana",
+    distance: 140,
+  },
+]
+let distanceByArrive = customerSchema.sort((a, b) => {
+  return a.distance - b.distance
 })
 
 console.log(distanceByArrive)
-var matrice = new Array()
 
 for (
   let index = 0;
-  index <= distanceByArrive[distanceByArrive.length - 1];
+  index <= distanceByArrive[distanceByArrive.length - 1]["distance"];
   index++
 ) {
-  matrice[index] = index
   let result = distanceByArrive.filter(
-    (distanceCurrent) => distanceCurrent == index
+    (distanceCurrent) => distanceCurrent.distance == index
   )
   if (result.length != 0) {
+    console.log(
+      `J'ai ${result.length} client${
+        result.length > 1 ? "s" : ""
+      } qui arrive a`,
+      index + "km"
+    )
+
     for (let j = 0; j < result.length; j++) {
       console.log(
-        `${j + 1}` + " " + `client${j > 1 ? "s" : ""} arrive`,
-        result[j] + "km"
+        `${j + 1})` + " " + result[j]["name"] + " " + ` arrive à`,
+        result[j]["destination"]
       )
     }
     console.log("=======================================")
